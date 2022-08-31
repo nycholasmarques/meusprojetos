b6cd599eb181936d586261884a77f40d4e077258
nycholasmarques <nycholasantonny@gmail.com>
2022-08-31 16:46:02 -0300

    slide criado

--- blog/js/code.js
+++ blog/js/code.js
@@ -3,22 +3,32 @@ let currentItem = 0;
 const items = document.querySelectorAll('.item');
 const maxItems = items.length;
 
-controls.forEach(control => {
+controls.forEach((control) => {
     control.addEventListener('click', () => {
-        const isLeft = control.classList.contains('.arrow-left');
-        console.log('control clicked', isLeft)
+        const isLeft = 
+        control.classList.contains('arrow-left');
 
         if (isLeft) {
             currentItem -= 1;
         } else {
             currentItem += 1;
         }
-    })
 
-    if (currentItem >= maxItems) {
-        currentItem < 0
-    }
-    if (currentItem < 0) {
-        currentItem = maxItems - 1;
-    }
+        if (currentItem >= maxItems) {
+            currentItem = 0
+        }
+
+        if (currentItem < 0) {
+            currentItem = maxItems - 1;
+        }
+        
+        items.forEach(item => item.classList.remove('current-item'));
+
+        items[currentItem].scrollIntoView({
+            inline: 'center',
+            behavior: "smooth"
+        });
+
+        items[currentItem].classList.add('current-item');
+    });
 });
